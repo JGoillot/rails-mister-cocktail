58bf30ac98814e7f17662956118a0ebbb989e9e0
JFachetti <goillot.jeremy@gmail.com>
2016-08-12 14:37:52 +0200

    Card and cocktail header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,18 @@
 module ApplicationHelper
 
+
+  def yield_with_default(holder, default)
+    if content_for?(holder)
+      content_for(holder).squish
+    else
+      default
+    end
+  end
+
+
   def cocktail_image(url)
     if url.nil?
-      "http://loremflickr.com/600/350/cocktail,alcohol,glass/all"
+      "http://loremflickr.com/560/300/cocktail,alcohol,glass/all"
     else
       cl_image_path(url.path, :width => 540, :height => 175, :crop => :fill)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,69 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Cocktail.destroy_all
 Ingredient.destroy_all
 
-puts "Seed Begin"
+ingredients = [
+'Angostura Bitter',
+'Bacardi Superior Rum',
+'Banana',
+'Beer',
+'Bitter Lemon',
+'Blackcurrant Cordial',
+'Blackcurrant Juice',
+'Brandy',
+'Brown Sugar',
+'Butter',
+'Champagne',
+'Chocolate',
+'Cider',
+'Cinnamon',
+'Cocoa Powder',
+'Coffee',
+'Cointreau',
+'Coke',
+'Cranberry Juice',
+'Cream',
+'Dark Rum',
+'Diet Coke',
+'Double Cream',
+'Egg white',
+'Gin',
+'Ginger Ale',
+'Grenadine',
+'Honey',
+'Hot Chocolate',
+'Lemon',
+'Lemon Juice',
+'Lemonade',
+'Lime',
+'Lime Juice',
+'Marmalade',
+'Milk',
+'Nutmeg',
+'Orange Juice',
+'Pepper',
+'Red Wine',
+'Rum',
+'Salt',
+'Sherry Sweet',
+'Soda Water',
+'Sparkling Water',
+'Sugar',
+'Sugar Syrup',
+'Sweet Vermouth',
+'Tabasco Sauce',
+'Tequila',
+'Tonic Water',
+'Triple Sec',
+'Vermouth',
+'Vodka',
+'Whisky',
+'White Rum',
+'White Wine',
+'Worcestershire Sauce'
+]
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
 
-puts "= Finish this Seed"
 
